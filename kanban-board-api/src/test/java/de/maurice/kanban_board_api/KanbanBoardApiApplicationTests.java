77d82cea692e0643aa83715afc84535a4b972c49
Maurice Beier <mauricebeier05@gmail.com>
2026-03-09 18:26:15 +0100

    Initial commit

---
+++ kanban-board-api/src/test/java/de/maurice/kanban_board_api/KanbanBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.maurice.kanban_board_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
